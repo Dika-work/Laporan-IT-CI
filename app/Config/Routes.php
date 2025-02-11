2ace5cc7d1405806e2194b0528b297f299603ad5
DikaWork <dikawork2@gmail.com>
2025-02-11 10:41:16 +0700

    Menambahkan prefix api untuk API routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,34 +7,30 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+$routes->group('api', function ($routes) {
+    $routes->get('users', 'UserController::index');
+    $routes->post('register-user', 'UserController::create');
+    $routes->put('users/(:hash)', 'UserController::update/$1');
+    $routes->delete('users/(:hash)', 'UserController::delete/$1');
+    $routes->post('login', 'UserController::login');
+    $routes->get('getDeHashUsername', 'UserController::getUsernameByHash');
 
-$routes->get('users', 'UserController::index');
-$routes->post('register-user', 'UserController::create');
-$routes->put('users/(:hash)', 'UserController::update/$1');
-$routes->delete('users/(:hash)', 'UserController::delete/$1');
-$routes->post('login', 'UserController::login');
-$routes->get('getDeHashUsername', 'UserController::getUsernameByHash');
-$routes->setAutoRoute(true); // Pastikan auto-routing diaktifkan
-$routes->get('uploads/(:any)', function ($path) {
-    return view('path/to/uploads/' . $path);
+    // laporan bug
+    $routes->get('getAllLaporan', 'LaporanBugController::index');
+    $routes->get('get-laporan-admin', 'LaporanBugController::getAllLaporan');
+    $routes->post('createLaporan', 'LaporanBugController::createLaporan');
+    $routes->post('updateLaporan', 'LaporanBugController::updateLaporan');
+    $routes->delete('deleteLaporan', 'LaporanBugController::deleteLaporan');
+    $routes->put('status-kerja', 'LaporanBugController::changeStatusKerja');
+
+    // Aplikasi category
+    $routes->get('getCategoryApk', 'ApkCategoryController::index');
+    $routes->post('createApkCategory', 'ApkCategoryController::createApkCategory');
+    $routes->delete('deleteApkCategory', 'ApkCategoryController::deleteApkCategory');
+
+    // Laporan pekerjaan
+    $routes->get('getLaporanPekerjaan', 'LaporanPekerjaanController::index');
+    $routes->post('laporan-pekerjaan', 'LaporanPekerjaanController::createLaporanPekerjaan');
+    $routes->put('update-pekerjaan', 'LaporanPekerjaanController::updatePekerjaan');
+    $routes->delete('delete-pekerjaan', 'LaporanPekerjaanController::deletePekerjaan');
 });
-
-
-// laporan bug
-$routes->get('getAllLaporan', 'LaporanBugController::index');
-$routes->get('get-laporan-admin', 'LaporanBugController::getAllLaporan');
-$routes->post('createLaporan', 'LaporanBugController::createLaporan');
-$routes->post('updateLaporan', 'LaporanBugController::updateLaporan');
-$routes->delete('deleteLaporan', 'LaporanBugController::deleteLaporan');
-$routes->put('status-kerja', 'LaporanBugController::changeStatusKerja');
-
-// Aplikasi category
-$routes->get('getCategoryApk', 'ApkCategoryController::index');
-$routes->post('createApkCategory', 'ApkCategoryController::createApkCategory');
-$routes->delete('deleteApkCategory', 'ApkCategoryController::deleteApkCategory');
-
-// Laporan pekerjaan
-$routes->get('getLaporanPekerjaan', 'LaporanPekerjaanController::index');
-$routes->post('laporan-pekerjaan', 'LaporanPekerjaanController::createLaporanPekerjaan');
-$routes->put('update-pekerjaan', 'LaporanPekerjaanController::updatePekerjaan');
-$routes->delete('delete-pekerjaan', 'LaporanPekerjaanController::deletePekerjaan');
